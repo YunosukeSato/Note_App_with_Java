09be12c436ded1e451d7151bda910900f32bdd99
Yunosuke Sato <yunosukesato04@gmail.com>
2022-11-23 11:41:10 -0800

    edited some login systems and sign up system

--- src/main/java/com/example/Note/controllers/LoginController.java
+++ src/main/java/com/example/Note/controllers/LoginController.java
@@ -19,21 +19,31 @@ public class LoginController {
 	private LoginModel loginModel;
 	
 	@GetMapping(value="/")
-	public String displayLogin() {
-//		model.addAttribute("loginRequest", new UserRequest());
+	public String displayLogin(Model model) {
+		model.addAttribute("loginRequest", new UserRequest());
 		return "/Login";
+	}
+	
+	@GetMapping(value="/moveSignUp")
+	public String displaySignUp(Model model) {
+		model.addAttribute("signUpRequest", new UserRequest());
+		return "/SignUp";
 	}
 	
 	@PostMapping(value="/SignUp")
 	public String signUp(@ModelAttribute UserRequest userRequest, Model model) {
-		if(userRequest.getUsername() == null || userRequest.getPassword() == null) {
-			model.addAttribute("loginRequest", new UserRequest());
-			return "/EmptyError";
+		if(loginModel.isExist(userRequest.getUsername()) != false) {
+			model.addAttribute("signUpRequest", new UserRequest());
+            return "/SignUpError";
+        }
+		if(userRequest.getUsername() == "" || userRequest.getPassword() == "") {
+			model.addAttribute("signUpRequest", new UserRequest());
+			return "/SignUpError";
 		}
 		
 		loginModel.signUp(userRequest.getUsername(), userRequest.getPassword());
 		model.addAttribute("loginRequest", new UserRequest());
-		return "/Login";
+		return "/Main";
 	}
 	
 	@GetMapping(value="/Login")
@@ -43,7 +53,7 @@ public class LoginController {
 			return "/EmptyError";
 		}
 		
-		if(!loginModel.isLogin(userRequest.getUsername(), userRequest.getPassword()) == false) {
+		if(loginModel.isLogin(userRequest.getUsername(), userRequest.getPassword()) == false) {
 			return "/LoginError";
 		}
 		
